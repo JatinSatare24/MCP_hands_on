d61a6700524e7c76d62a57a7e2c8b742e1c63352
Jatin Satare <jatinsatare@gmail.com>
2026-09-21 19:36:47 +0530

    create a new mcp server

--- server.ts
+++ server.ts
@@ -2,3 +2,12 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { z } from "zod";
 
+// Create MCP server instance
+const server = new McpServer({
+  name: 'Weather Data Fetcher',
+  version: '1.0.0'
+
+});
+
+
+
